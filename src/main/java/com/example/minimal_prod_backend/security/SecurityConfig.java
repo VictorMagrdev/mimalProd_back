9cbb838657038139589abe52483babbac7d27703
VictorMagrdev <victorakiladev@gmail.com>
2025-12-02 11:40:27 -0500

    configuracion de cors

--- src/main/java/com/example/minimal_prod_backend/security/SecurityConfig.java
+++ src/main/java/com/example/minimal_prod_backend/security/SecurityConfig.java
@@ -50,7 +50,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://minimalprod.victorakiladev.workers.dev","https://minimalprod.space"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://minimalprod.victorakiladev.workers.dev",
+                "https://minimalprod.space",
+                "https://*.victorakiladev.workers.dev",
+                "https://*.minimalprod.space",
+                "https://minimalprod.space"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);

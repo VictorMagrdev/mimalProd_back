4ba6aae1335c71b3b063314de75bc9cdf30df3a4
VictorMagrdev <victorakiladev@gmail.com>
2025-12-02 17:52:04 -0500

    configuracion de cors

--- src/main/java/com/example/minimal_prod_backend/security/SecurityConfig.java
+++ src/main/java/com/example/minimal_prod_backend/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-               // .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+               .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auths -> auths
